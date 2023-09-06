035f430428665e8a90c31dd37653e9b2ad40d04c
Casey <141671253+FaceOfSpades@users.noreply.github.com>
2023-09-06 14:57:21 -0400

    Create code to get game choices and compare them
    created function that gets the players choice and makes sure its valid
    created another function that gets the computers choice
    created another function that plays a round by comparing the two and
    returning the correct string value based off the result

--- script.js
+++ script.js
@@ -14,4 +14,41 @@ function getComputerChoice() {
    return compChoice;
 }
 
-let compPlay = getComputerChoice();
+function getPlayerChoice() {
+    let playChoice = prompt("Let's play Rock, Paper, Scissors. Please enter your choice: ");
+    playChoice = playChoice.toLowerCase();
+
+    let choice = "false";
+
+    while (choice === "false"){
+        if (playChoice === "rock" || playChoice === "paper" || playChoice === "scissors") {
+            choice = "true";
+            return playChoice;
+        } else playChoice = prompt("That is not a valid choice, please try again: ");  
+    }  
+}
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return "It's a Tie!";
+    } else if (playerSelection === "rock" && computerSelection === "paper") {
+        return "You lose, paper beats rock!";
+    } else if (playerSelection === "rock" && computerSelection === "scissors") {
+        return "You win, rock beats scissors!";
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+        return "You win, paper beats rock!";
+    } else if (playerSelection === "paper" && computerSelection === "scissors") {
+        return "You lose, scissors beats paper!";
+    } else if (playerSelection === "scissors" && computerSelection === "rock") {
+        return "You lose, rock beats scissors!";
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        return "You win, scissors beats paper!";
+    } 
+}
+
+playRound(getPlayerChoice(), getComputerChoice());
+
+
+
+
+
