05e0aea769c08caea21d941631a36c9dfebd440a
Casey <141671253+FaceOfSpades@users.noreply.github.com>
2023-10-03 16:21:36 -0400

    Rework code to use buttons and DOM methods
    removed code for console.log and alert prompts and added in buttons
    with even listeners that would pass the choice into the playRound
    function
    also added results section to display the running score of the game
    and announce a winner for the first player to reach 5 points

--- script.js
+++ script.js
@@ -14,64 +14,83 @@ function getComputerChoice() {
    return compChoice;
 }
 
-function getPlayerChoice() {
-    let playChoice = prompt("Let's play 5 rounds of Rock, Paper, Scissors. Please enter your choice: ");
-    playChoice = playChoice.toLowerCase();
+function playRound(playerSelection) {
+    let computerSelection = getComputerChoice();
+    let result = "";
 
-    let choice = "false";
-
-    while (choice === "false"){
-        if (playChoice === "rock" || playChoice === "paper" || playChoice === "scissors") {
-            choice = "true";
-            return playChoice;
-        } else playChoice = prompt("That is not a valid choice, please try again: ");  
-    }  
-}
-
-function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
-        return "It's a Tie!";
+        result = "It's a Tie!";
     } else if (playerSelection === "rock" && computerSelection === "paper") {
-        return "You lose, paper beats rock!";
+        result = "You lose, paper beats rock!";
     } else if (playerSelection === "rock" && computerSelection === "scissors") {
-        return "You win, rock beats scissors!";
+        result = "You win, rock beats scissors!";
     } else if (playerSelection === "paper" && computerSelection === "rock") {
-        return "You win, paper beats rock!";
+        result = "You win, paper beats rock!";
     } else if (playerSelection === "paper" && computerSelection === "scissors") {
-        return "You lose, scissors beats paper!";
+        result = "You lose, scissors beats paper!";
     } else if (playerSelection === "scissors" && computerSelection === "rock") {
-        return "You lose, rock beats scissors!";
+        result = "You lose, rock beats scissors!";
     } else if (playerSelection === "scissors" && computerSelection === "paper") {
-        return "You win, scissors beats paper!";
+        result = "You win, scissors beats paper!";
     } 
+    return result;
 }
+ let winCount = 0;
+ let lossCount = 0;
+ let result = "";
+   
+const buttons = document.querySelectorAll("button");
+const div = document.querySelector("div");
+const results = document.querySelector("#results");
+const p = document.createElement("p");
+const p2 = document.createElement("p");
+const gameResult = document.createElement("h2");
+const roundResult = document.createElement("h2");
+const reset = document.createElement("button");
+reset.textContent = "Reset";
 
-function game() {
-    let winCount = 0;
-    let lossCount = 0;
-    let result;
-    for (let i = 0; i < 5; i++) {
-        let result = playRound(getPlayerChoice(), getComputerChoice());
-        console.log(result);
-        if (result === "You win, rock beats scissors!" || result === "You win, scissors beats paper!" || result === "You win, paper beats rock!") {
+buttons.forEach( (e) => {
+    e.addEventListener("click", () => {
+        roundResult.remove();
+        result = e.innerHTML;
+        result = result.toLowerCase();
+        let winner = playRound(result);
+
+        if (winner === "You win, rock beats scissors!" || winner === "You win, scissors beats paper!" || winner === "You win, paper beats rock!") {
             winCount++;
-        } else if (result === "It's a Tie!") {
-            winCount++;
-            lossCount++;
-        } else lossCount++;
-    }
-    if (winCount > lossCount) {
-        result = "Congratulations, you won the game!";
-    } else if (winCount === lossCount) {
-        result = "You didn't win or lose, its a tie.";
-    } else result = "Sorry, you lost. Better luck next time.";
+        } else if (winner === "It's a Tie!"){
 
-    console.log(result);
-}
+        }
+        else lossCount++;
 
-game();
+        roundResult.textContent = winner;
+        results.appendChild(roundResult);
 
+        p.textContent = "Your score: " + winCount;
+        p2.textContent = "Computer score: " + lossCount;
+        div.appendChild(p);
+        div.appendChild(p2);
 
+        if (winCount == 5) {
+            gameResult.textContent = "Congratulations, you were the first to 5. You won the game!";
+            results.appendChild(gameResult);
+            div.appendChild(reset);
+        }
 
+        if (lossCount == 5) {
+            gameResult.textContent = "Sorry, you lost. Your opponent scored 5 points first.";
+            results.appendChild(gameResult);
+            div.appendChild(reset);
+        }
 
-
+        reset.addEventListener("click", () => {
+            winCount = 0;
+            lossCount = 0;
+            reset.remove();
+            p.remove();
+            p2.remove();
+            gameResult.remove();
+            roundResult.remove();
+        })
+    })
+})
